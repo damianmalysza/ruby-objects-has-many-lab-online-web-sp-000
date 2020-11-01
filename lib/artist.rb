ea153dbd30fea9d033cd079cf19d42b816dd37d7
Damian Malysza <damian.malysza@gmail.com>
2020-11-01 18:37:07 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    @songs = Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+end
